b8c711b7d7592649645b9e57a342c5bc135b6085
Mizsei Tamás <72611482+toxisgen@users.noreply.github.com>
2021-05-20 20:56:10 +0200

    Update Hilbert.v

--- Hilbert.v
+++ Hilbert.v
@@ -12,8 +12,8 @@ Inductive Hilbert_proof_sys (Γ : (list form)): form -> Prop :=
 | P2F (φ ψ ξ : form)        : Γ ⊢_FOL (((φ --> (ψ --> ξ))) --> ((φ --> ψ) --> (φ --> ξ)))
 | P3F (φ : form)            : Γ ⊢_FOL (((φ --> fal ) --> fal )--> φ)
 | MPF (φ1 φ2 : form)        : Γ ⊢_FOL φ1 -> Γ ⊢_FOL (φ1 --> φ2) -> Γ ⊢_FOL φ2
-| Q5F (Φ : form) (t : term) : Γ ⊢_FOL (Φ[t..]) ->  Γ ⊢_FOL (∃ Φ) (* TODO: Ask if it's a good instantiate *)
-| Q6F (Φ ψ : form)          : Γ ⊢_FOL ((Φ --> ψ)) ->  Γ ⊢_FOL ((∃ Φ) --> ψ)
+| Q5F (Φ : form) (t : term) : Γ ⊢_FOL (Φ[t..]) ->  Γ ⊢_FOL (∃ Φ)
+| Q6F (Φ ψ : form)(x : nat) : Γ ⊢_FOL ((Φ[($x)..] --> ψ)) ->  Γ ⊢_FOL ((∃ Φ) --> ψ)
 where "Γ ⊢_FOL form" := (Hilbert_proof_sys Γ form).
 
 (* TODO: test this *)
